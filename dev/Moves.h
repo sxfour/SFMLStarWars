1ecb15b91d86c628933a9d460b8cfbbe702e9219
Egor Levashov <112577182+sxfour@users.noreply.github.com>
2023-05-31 11:45:42 +0300

    Update Moves.h

--- dev/Moves.h
+++ dev/Moves.h
@@ -1,10 +1,8 @@
-#pragma once
-
 #include <SFML/Graphics.hpp>
-#include <iostream>
 
 using namespace sf;
 
-void playerMove(Sprite& playerSprite, Vector2f & moveRec, Text& playerText);
+void playerMove(Sprite& playerSprite, Vector2f& moveRec, Text& playerText);
 void enemy1Move(Sprite& playerSprite, Sprite& enemy1Sprite, Vector2f& moveEnemy1, int& lifes, Text& enemy1Text);
 void enemy2Move(Sprite& playerSprite, Sprite& enemy2Sprite, Vector2f& moveEnemy2, int& lifes, Text& enemy2Text);
+void meteorMove(Sprite& meteorSprite, Vector2f& moveMeteor, Text& meteorText);
